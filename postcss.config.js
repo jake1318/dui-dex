36927da984959480f3e4ebb0a5d077d66e749440
jake1318 <106035392+jake1318@users.noreply.github.com>
2025-01-25 20:01:32 -0500

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+/**
+ * @file postcss.config.js
+ * Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2024-01-26 00:59:32
+ * Current User's Login: jake1318
+ */
+
 export default {
   plugins: {
-    tailwindcss: {},
     autoprefixer: {},
   },
 };
